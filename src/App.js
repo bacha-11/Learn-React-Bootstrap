7b07d11783b20de29ad0ce9e95c871e203a525e9
bacha-11 <ijazbacha14@gmail.com>
2021-09-22 16:39:18 +0500

    take same button as a example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Col, Row } from 'react-bootstrap';
+
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App mx-0">
+      <Row className="">
+        <Button as={Col} variant="primary" >Click Me</Button>
+        <Button as={Col} variant="success" className="mx-2" >Click Me</Button>
+        <Button as={Col} variant="secondary" >Click Me</Button>
+        <Button as={Col} className="mine mx-2">Click Me</Button>
+      </Row>
     </div>
   );
 }
