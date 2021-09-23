c90e95f1ea3f35f7b60160b4f7b28448c9ba0255
bacha-11 <ijazbacha14@gmail.com>
2021-09-23 22:40:48 +0500

    responsive in bootstrap

--- src/components/pages/LargeContainerWithRowColum.js
+++ src/components/pages/LargeContainerWithRowColum.js
@@ -5,9 +5,9 @@ function LargeContainerWithRowColumn() {
     return (
         <div className="conatainer-fluit bg-primary">
             <div className="row bg-dark">
-                <div className="col-lg-6 bg-success">col-lg-6</div>
-                <div className="col-lg-4 bg-secondary">col-lg-4</div>
-                <div className="col-lg-2 bg-success">col-lg-2</div>
+                <div className="col-lg-6 col-xl-4 col-md-4 col-sm-6 col-6 bg-success">col-lg-6</div>
+                <div className="col-lg-4 col-xl-6 col-md-4 col-sm-6 col-6 bg-secondary">col-lg-4</div>
+                <div className="col-lg-2 col-md-4 col-sm-4 bg-success">col-lg-2</div>
             </div>
         </div>
     )
