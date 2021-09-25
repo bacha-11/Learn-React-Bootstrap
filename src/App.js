4e8ab861516535d7a4d3ed3ba152f960abf04215
bacha-11 <ijazbacha14@gmail.com>
2021-09-25 11:41:08 +0500

    auto same column in bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AutoColumAndRow from './components/pages/AutoColumAndRow';
 import ContainerExample from './components/pages/ContainerExample';
 import LargeContainerWithRowColumn from './components/pages/LargeContainerWithRowColum';
 import NestedColumn from './components/pages/NestedColumn';
@@ -13,7 +14,9 @@ function App() {
 
       <LargeContainerWithRowColumn /> */}
 
-      <NestedColumn />
+      {/* <NestedColumn /> */}
+
+      <AutoColumAndRow />
     </div>
   );
 }
---
+++ src/components/pages/AutoColumAndRow.js
@@ -0,0 +1,14 @@
+
+function AutoColumAndRow(){
+    return(
+        <div className="container-fluit bg-dark">
+            <div className="row">
+                <div className="col bg-success">div1</div>
+                <div className="col bg-info">div2</div>
+                <div className="col bg-warning">div3</div>
+            </div>
+        </div>
+    )
+}
+
+export default AutoColumAndRow;
