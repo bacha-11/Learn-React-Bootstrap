c3e41a4636eed0afe879ffbc555169ce0e0d0335
bacha-11 <ijazbacha14@gmail.com>
2021-09-25 19:15:03 +0500

    text-center, rounded property in bootstrap

--- src/components/pages/Offset.js
+++ src/components/pages/Offset.js
@@ -4,8 +4,8 @@ function Offset(){
     return(
         <div className="container-fluit text-white">
             <div className="row bg-dark">
-                <div className="col-4 offset-1 bg-warning ">div1</div>
-                <div className="col-4 offset-2 bg-success ">div2</div>
+                <div className="col-4 offset-1 bg-warning text-center rounded">div1</div>
+                <div className="col-4 offset-2 bg-success text-end ">div2</div>
                 
                 
             </div>
